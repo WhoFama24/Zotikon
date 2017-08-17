a1b4fc4dbd925cdc3376919d6d4f95092d7e6f9e
efarmer <ericdfarmer988@gmail.com>
2017-08-17 16:01:58 -0500

    Initial file structure.

---
+++ development/modules/wireless/hello_world.py
@@ -0,0 +1,3 @@
+# This program is designed to test the basic communication ability of the Synapse SNAP modules.
+# The program should be able to send a string to the remote Synapse module and have it returned as the reverse of the
+# received string and displayed in the console.
