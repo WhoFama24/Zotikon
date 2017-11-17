3fe57673d04a48be2c76f34eb675f10ba374e82e
Bruce <bab615@msstate.edu>
2017-11-17 16:06:29 -0600

    adding comments and instructions on how to setup up the synapse board. removing some unnecessary functions.

--- development/modules/wireless/HeartRateAndTempReadRF200.py
+++ development/modules/wireless/HeartRateAndTempReadRF200.py
@@ -1,8 +1,35 @@
+"""HeartRateAndTempReadRF200.py
+
+    This file is used to gathering heart rate and temperature on the synapse RF200 devices. In order to set this up
+    you should do the following:
+
+    Basic set up:
+        make sure that you have your circuits and pieces powered by the same ground and power on the breadboard.
+
+    Temperature:
+        Hook up the ds1631 on a bread board. Hook up A0, A1, and A2 to ground for a 000 I2C address. Connect the SDA
+        pin to GPIO 17 on the breakout board. connect the SCL pin to GPIO 18 on the breakout board.
+
+        see more at https://developer.synapse-wireless.com/modules/rf200/port-mapping.html#i2c
+
+        You can run ds1631_temp_read or get_data_string() on portal to see this output.
+
+
+    Heart rate:
+        For now all the heart rate function does is read the ADC pin and give a value between 0 and 1023. This is a
+        10 bit ADC and has an internal VREF of 1.6 volts. In order to get a valid reading make sure you are using the
+        same ground from the volatge you are reading. Hook up the GPIO 11 pin on the breakout board to the voltage
+        source you want to read.
+
+        You can run ds1631_temp_read or get_data_string() on portal to see this output.
+
+        see more at https://developer.synapse-wireless.com/modules/atmega/builtins.html#readadc"""
+
 from synapse.platforms import *
 from synapse.switchboard import *
 
+# used for heartbeat LED
 GREEN_LED = GPIO_1
-YELLOW_LED = GPIO_2
 
 melexisTemp = []
 ds1631Temp = []
@@ -11,9 +38,8 @@ heartRate = []
 
 @setHook(HOOK_STARTUP)
 def startup():
-    """Initialize LED pins to output"""
+    """Initialize LED pins to output. Set up I2C and tell the ds1631 to start reading temperature"""
     setPinDir(GREEN_LED, True)
-    setPinDir(YELLOW_LED, True)
     i2cInit(True)
 
     # tell the ds1631 to start calculating temperature
@@ -72,22 +98,6 @@ def heart_rate_read():
     # return heart_voltage
 
     return heart_voltage
-
-
-def get_heart_rate():
-    """Return heart rate and temperature data collected by this athlete worn device."""
-
-    # global heartRate, ds1631Temp
-
-    return heart_rate_read()
-
-
-def get_temperature():
-    """Return heart rate and temperature data collected by this athlete worn device."""
-
-    # global heartRate, ds1631Temp
-
-    return ds1631_temp_read()
 
 
 def get_data_string():
